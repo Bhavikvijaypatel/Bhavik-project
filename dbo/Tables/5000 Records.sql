aa8753083c2ef7292c0ddd2811f7c2cc73707ce9
Bhavik <Bhavik3926@icloud.com>
2024-07-25 12:51:15 -0500

    On branch main
     Initial commit
    
     Changes to be committed:
    	new file:   Bhavik project.sqlproj
    	new file:   dbo/Tables/5000 Records.sql
    	new file:   dbo/Tables/Test.sql

---
+++ dbo/Tables/5000 Records.sql
@@ -0,0 +1,17 @@
+
+
+SELECT
+
+ROW_NUMBER() OVER (
+
+ORDER BY Emp_ID
+
+) row_num,
+
+*
+
+FROM
+
+dbo.[5000 Records];
+
+select * from dbo.[5000 Records]
---
+++ dbo/Tables/Test.sql
@@ -0,0 +1,10 @@
+CREATE TABLE [dbo].[Test] (
+    [Id]       INT           NOT NULL,
+    [Name]     NVARCHAR (50) NOT NULL,
+    [Lastname] NVARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+GO
+
