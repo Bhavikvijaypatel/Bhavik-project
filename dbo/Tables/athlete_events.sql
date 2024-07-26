1eae5c3dcab964ed1d019122a886de218de19fdf
Bhavik <Bhavik3926@icloud.com>
2024-07-26 12:23:37 -0500

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   dbo/Tables/athlete_events.sql

--- dbo/Tables/athlete_events.sql
+++ dbo/Tables/athlete_events.sql
@@ -1,19 +1,2 @@
-CREATE TABLE [dbo].[athlete_events] (
-    [ID]     NVARCHAR (50)  NOT NULL,
-    [Name]   NVARCHAR (MAX) NOT NULL,
-    [Sex]    NVARCHAR (50)  NOT NULL,
-    [Age]    NVARCHAR (50)  NULL,
-    [Height] NVARCHAR (50)  NULL,
-    [Weight] NVARCHAR (50)  NULL,
-    [Team]   NVARCHAR (50)  NOT NULL,
-    [NOC]    NVARCHAR (50)  NOT NULL,
-    [Games]  NVARCHAR (50)  NOT NULL,
-    [Year]   NVARCHAR (50)  NOT NULL,
-    [Season] NVARCHAR (50)  NOT NULL,
-    [City]   NVARCHAR (50)  NOT NULL,
-    [Sport]  NVARCHAR (50)  NOT NULL,
-    [Event]  NVARCHAR (100) NOT NULL,
-    [Medal]  NVARCHAR (50)  NOT NULL
-);
-GO
+select COUNT(ID) as total_rows from athlete_events
 
